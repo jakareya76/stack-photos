7610b37a4ea5611c679c06d1e3a1913e56e8d7ba
jakareya76 <jakareya1306@gmail.com>
2022-11-24 19:01:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos`;
+const client_id = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [newImages, setNewImages] = useState(false);
+
+  const mounted = useRef(false);
+
+  const fetchImages = async () => {
+    setLoading(true);
+
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${client_id}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${client_id}${urlPage}`;
+    }
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setNewImages(false);
+      setLoading(false);
+    } catch (error) {
+      setNewImages(false);
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!query) return;
+
+    if (page === 1) {
+      fetchImages();
+      return;
+    }
+    setPage(1);
+  };
+
+  useEffect(() => {
+    fetchImages();
+    // eslint-disable-next-line
+  }, [page]);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+
+    if (!newImages) return;
+    if (loading) return;
+
+    setPage((oldPage) => oldPage + 1);
+    // eslint-disable-next-line
+  }, [newImages]);
+
+  const scrollEvent = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollEvent);
+    return () => window.removeEventListener("scroll", scrollEvent);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo, index) => {
+            return <Photo key={index} {...photo} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </main>
   );
 }
 
